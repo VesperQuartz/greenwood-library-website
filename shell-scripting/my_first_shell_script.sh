6b1731d45acc0423e617751b1674e396be6a9ef6
mrlectus <lilbrown3000@gmail.com>
2025-06-30 13:57:13 +0100

    added assets

--- shell-scripting/my_first_shell_script.sh
+++ shell-scripting/my_first_shell_script.sh
@@ -1,4 +1,5 @@
-# create user
-sudo useradd user1
-sudo useradd user2
-sudo useradd user3
+#!/bin/env sh
+# This is another way to create
+# a multi-line comment. Each line
+# is prefixed with a # symbol.
+echo "Here is an actual code that gets executed"

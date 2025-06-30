b6bdce11fa0118851d1af4d1c2df930ddcb3978d
mrlectus <lilbrown3000@gmail.com>
2025-06-30 14:19:52 +0100

    added assets (1)

--- shell-scripting/my_first_shell_script.sh
+++ shell-scripting/my_first_shell_script.sh
@@ -8,3 +8,15 @@ This is a multi-line comment.
 It can span multiple lines and is ignored by the shell.
 This method is often used for larger blocks of documentation.
 END_COMMENT
+
+# This script demonstrates how to use comments in Bash scripting
+
+
+echo "Welcome" # welcome messae
+
+mkdir TestFolder #To make a test dir
+
+ls -lah #To list dir
+
+echo "Good bye" # goodbye message
+

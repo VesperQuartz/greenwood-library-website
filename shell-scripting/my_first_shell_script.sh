fced9412a8b606dad061ca82a470a57fd0b4d7d3
mrlectus <lilbrown3000@gmail.com>
2025-06-30 14:14:17 +0100

    added assets

--- shell-scripting/my_first_shell_script.sh
+++ shell-scripting/my_first_shell_script.sh
@@ -2,4 +2,9 @@
 # This is another way to create
 # a multi-line comment. Each line
 # is prefixed with a # symbol.
-echo "Here is an actual code that gets executed"
+#
+: <<'END_COMMENT'
+This is a multi-line comment.
+It can span multiple lines and is ignored by the shell.
+This method is often used for larger blocks of documentation.
+END_COMMENT
